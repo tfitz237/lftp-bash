14818c742dfb1d8539e14a7badbbb47f1e2287b4
Tom Fitzpatrick <tfitz237@gmail.com>
2016-09-13 21:13:46 -0400

    Updated ftp.sh
    Created functions and case statements, saving passwords is now optional. Now allows full path as well as relative path for REMOTEFOLDER

--- ftp.sh
+++ ftp.sh
@@ -1,57 +1,121 @@
 #!/bin/bash
-if [[ $# -eq 0 ]] ; then
-    echo 'Please enter a directory name'
-    exit 0
-fi
-PREFIX='lftp-bash'
+helpme() {
+echo "<< LFTP-BASH >>
+Usage:  ftp.sh DIR ACTION [options]
+    
+DIR:    Directory Name
+Action: 
+        -h, --help      Display this message
+        -d, --download  Download from server (default)
+        -u, --upload    Upload to server
+        -c, --config    FTP Configuration 
+Options:
+        --delete        Enables deletion of files not in sync with local files. Used in combination of -u"
+}
 
-DIR=${1%/}
-if [ ! -d "$PREFIX-config" ]; then
-    mkdir -p $PREFIX-ftp-config
-fi
-if [ ! -e "$PREFIX-config/$DIR-config.sh" ] ; then
-    echo "<<First time setup for FTP site>>"
-    read -e -p "FTP Host: " HOST
-    read -e -p "FTP User: " USER
-    read -e -p "FTP Password: " PASS
-    read -e -p "Remote Directory: " REMOTEFOLDER
-    read -e -p "Local Path: " -i "$PWD/" LOCALFOLDER 
-    echo "HOST='$HOST'
-USER='$USER'
+config() {
+    echo "<< FTP Configuration >>
+-----------------------------"
+    read -e -p "FTP Host: " -i "$HOST" HOST
+    read -e -p "FTP User: " -i "$USR" USR
+    read -e -p "Remote Directory: " -i "$REMOTEFOLDER" REMOTEFOLDER
+    if [ -z $LOCALFOLDER]; then 
+        LOCALFOLDER="$PWD/"
+    fi
+    read -e -p "Local Path: " -i "$LOCALFOLDER" LOCALFOLDER     
+    read -e -p "Save Password? (yes/y/no/n): " -i "$SAVE" SAVE
+    if [ "$SAVE" = "y" ] || [ "$SAVE" = "yes" ]; then
+        read -e -p "FTP Password: " -i "$PASS" PASS
+        echo "HOST='$HOST'
+USR='$USR'
 PASS='$PASS'
 REMOTEFOLDER='$REMOTEFOLDER'
 LOCALFOLDER='$LOCALFOLDER'
-    " >> "$PREFIX-config/$DIR-config.sh"
+SAVE='$SAVE'" > "$PREFIX-config/$DIR-config.sh"
+    else
+        echo "HOST='$HOST'
+USR='$USR'
+REMOTEFOLDER='$REMOTEFOLDER'
+LOCALFOLDER='$LOCALFOLDER'
+SAVE='$SAVE'" > "$PREFIX-config/$DIR-config.sh"
+    fi
+}
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Please enter a directory name. -h for help'
+    exit 0
+fi
+if [[ $1 == -* ]]; then
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    helpme
+    fi
+    exit 0
+fi
+
+PREFIX='lftp-bash'
+DIR=${1%/}
+
+if [ ! -d "$PREFIX-config" ]; then
+    mkdir -p $PREFIX-config
+fi
+if [ ! -e "$PREFIX-config/$DIR-config.sh" ] ; then
+    config
     if [ ! -d "$DIR" ]; then
         mkdir -p $LOCALFOLDER/$DIR
     fi
-echo "--------------------------------------"
+echo "-----------------------------"
 else
-source "$PREFIX-config/$DIR-config.sh"
+    source "$PREFIX-config/$DIR-config.sh"
 fi
 
+download() 
+{
+    echo "<< Syncing from Remote --> Local >>
+-----------------------------"
+    lftp -e "
+    open $HOST
+    user $USR $PASS
+    lcd $LOCALFOLDER
+    mirror --delete --verbose --only-newer $REMOTEFOLDER $DIR/
+    bye
+    " 
+    echo "Sync Complete."    
+}
 
-if [ "$2" = "up" ] 
-then
-echo "Syncing from Local --> Remote
-------------------------------"
-lftp -e "
-open $HOST
-user $USER $PASS
-lcd $LOCALFOLDER 
-mirror --reverse --delete --verbose --only-newer $DIR/$REMOTEFOLDER $REMOTEFOLDER
-bye
-"
-echo "Done."
-else
-echo "Syncing from Remote --> Local
-------------------------------"
-lftp -e "
-open $HOST
-user $USER $PASS
-lcd $LOCALFOLDER
-mirror --delete  --verbose --only-newer $REMOTEFOLDER $DIR/
-bye
-" 
-echo "Done."
-fi
+upload() 
+{
+    echo "<< Syncing from Local --> Remote >>
+-----------------------------"
+    if [ "$3" = "--delete" ]; then
+        lftp -e "
+        open $HOST
+        user $USR $PASS
+        lcd $LOCALFOLDER 
+        mirror --reverse --delete --verbose --only-newer $DIR/$(basename $REMOTEFOLDER)/ $REMOTEFOLDER
+        bye
+        "
+    else
+        lftp -e "
+        open $HOST
+        user $USR $PASS
+        lcd $LOCALFOLDER 
+        mirror --reverse --verbose --only-newer $DIR/$(basename $REMOTEFOLDER)/ $(dirname $REMOTEFOLDER)/
+        bye
+        "
+    fi
+    echo "Sync Complete."
+}
+
+
+case "$2" in
+-h|--help)
+helpme;;
+-c|--config)
+config;;
+-u|--upload)
+upload;;
+-d|--download|*)
+download
+esac
+
+
