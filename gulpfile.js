a409ea89997e2fdb0a9e21da42e2711a7f3fa049
Tom Fitzpatrick <tfitz237@gmail.com>
2016-09-13 21:27:39 -0400

    Update gulpfile.js
    Added task for deleting remote files based on local directory.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,22 @@ var gulp = require('gulp');
 var sys = require('sys')
 var exec = require('child_process').exec;
 
+var localfolder = "/home/ubuntu/workspace/";
+
+
 gulp.task('default', function() {
     var watch = gulp.watch('**', function(event) {
-        var dir = event.path.replace("/home/ubuntu/workspace/",'');
+        var dir = event.path.replace(localfolder,'');
         dir = dir.substr(0, dir.lastIndexOf("/")+ 1);
-        exec("bash ftp.sh "+ dir + " up", puts); 
+        exec("bash ftp.sh "+ dir + " -u", puts); 
+    });
+});
+
+gulp.task('sync-delete', function() {
+    var watch = gulp.watch('**', function(event) {
+        var dir = event.path.replace(localfolder,'');
+        dir = dir.substr(0, dir.lastIndexOf("/")+ 1);
+        exec("bash ftp.sh "+ dir + " -u --delete", puts); 
     });
 });
 
